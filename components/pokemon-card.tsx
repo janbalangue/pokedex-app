294d4b2af2931b36e976b498b1ad56b8ee3b1a51
Jan Balangue <jan.a.balangue@gmail.com>
2025-05-22 18:07:42 -0700

    add colors to pokemon card types

--- components/pokemon-card.tsx
+++ components/pokemon-card.tsx
@@ -1,19 +1,49 @@
 import { Pokemon } from '@/utils/types';
 import Image from 'next/image';
 
+const typeColors: { [key: string]: string } = {
+  fire: 'bg-red-500',
+  water: 'bg-blue-500',
+  grass: 'bg-green-500',
+  electric: 'bg-yellow-400',
+  ice: 'bg-cyan-500',
+  fighting: 'bg-purple-500',
+  poison: 'bg-purple-700',
+  ground: 'bg-yellow-700',
+  flying: 'bg-blue-300',
+  psychic: 'bg-pink-500',
+  bug: 'bg-green-600',
+  rock: 'bg-yellow-800',
+  ghost: 'bg-purple-900',
+  dragon: 'bg-red-700',
+  dark: 'bg-gray-800',
+  fairy: 'bg-pink-300',
+  normal: 'bg-gray-600',
+  steel: 'bg-gray-400'
+}
+
 export default function PokemonCard({ name, imageUrl, id, types }: Pokemon) {
   return (
-    <div className="bg-gray-800 rounded-lg transition-transform duration-200 hover:scale-105 ring-2 ring-gray-700">
-      <Image
-        className="rounded-full"
-        src={imageUrl}
-        alt={name}
-        width={250}
-        height={250}
-      />
-      <h2 className="mt-2 font-semibold text-center">{name}</h2>
-      <p className="text-center text-gray-500">{id}</p>
-      <div className="text-center text-gray-400">{types.join(', ')}</div>
+    <div className="bg-gray-800 rounded-lg shadow-lg text-center transform transition-transform duration-200 hover:scale-105 ring-2 ring-gray-700 p-4">
+      <div className="relative aspect-square mb-4">
+        <Image
+          className="rounded-full"
+          src={imageUrl}
+          alt={name}
+          width={250}
+          height={250}
+        />
+      </div>
+      <h2 className="text-xl font-bold capitalize">{name}</h2>
+      <p className="text-gray-500 text-sm mb-1">#{String(id).padStart(3, '0')}</p>
+      {types.map((type, idx) => (<span
+        key={`${idx}-${type}`}
+        className={`text-white mx-1 px-2 py-1 rounded-full text-sm font-semibold ${typeColors[type]}`}
+        style={{ backgroundColor: typeColors[type] }}
+        title={type}
+      >
+        {type}
+      </span>))}
     </div>
   );
 }
