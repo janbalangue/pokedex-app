b0c32b24c8a48af1f173680d36131d44a221a2ad
Jan Balangue <jan.a.balangue@gmail.com>
2025-05-22 20:37:30 -0700

    edit next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const repo = "janbalangue/pokedex-app"; // <-- Replace with your GitHub repo name
+
 const nextConfig: NextConfig = {
+  output: "export",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
@@ -9,6 +13,8 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 };
 
 export default nextConfig;
